62d081f7f629082dc9eb24c2a456c68916e0bffd
Heli Sivunen <heli@mooncat.tv>
2022-01-12 17:13:25 +0000

    add playground notes in app.js

--- src/App.js
+++ src/App.js
@@ -15,3 +15,39 @@ function Display(props) {
 
 
 export default Button;
+
+
+// Code from Pluralsight exercise (playground):
+// function Button(props) {
+//   const handleClick = () => props.onClickFunction(props.increment)
+//   return (
+//     <button onClick={handleClick}>
+//       +{props.increment}
+//     </button>
+//   );
+// }
+//
+// function Display(props) {
+//   return (
+//     <div>{props.message}</div>
+//   );
+// }
+//
+// function App() {
+//   const [counter, setCounter] = useState(0);
+//   const incrementCounter = (incrementValue) => setCounter(counter+incrementValue)
+//   return (
+//     <div>
+//       <Button onClickFunction={incrementCounter} increment={1} />
+//       <Button onClickFunction={incrementCounter} increment={5} />
+//       <Button onClickFunction={incrementCounter} increment={10} />
+//       <Button onClickFunction={incrementCounter} increment={100} />
+//     <Display message={counter}/>
+//     </div>
+//   );
+// }
+//
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('mountNode'),
+// );
